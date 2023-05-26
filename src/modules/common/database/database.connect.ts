f8753a5b8a697b29622fde14c124f48e8596d166
skuma274 <sandeep.e.kumar@capgemini.com>
2023-05-26 13:59:55 +0530

    dedicated moduel for migration

--- src/modules/common/database/database.connect.ts
+++ src/modules/common/database/database.connect.ts
@@ -1,16 +1,9 @@
-import path from 'path';
 import { Sequelize } from 'sequelize-typescript';
-import { SequelizeTypescriptMigration } from 'sequelize-typescript-migration-lts';
 import { PinCode } from '../entities/pincode.entity';
 import { DatabaseService } from './database.service';
 
 export const sequelizeConnect = async (databaseService: DatabaseService):Promise<Sequelize> => {
     const sequelize = new Sequelize(databaseService.sequelizeOrmConfig);
-      sequelize.addModels([PinCode]);
-      await SequelizeTypescriptMigration.makeMigration(sequelize, {
-        outDir: path.resolve('db','migrations'),
-        migrationName: "migration",
-        preview: false,
-      });
-      return sequelize;
+    sequelize.addModels([PinCode]);
+    return sequelize;
 }
