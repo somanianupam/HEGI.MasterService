fd7280d5e5ddc62016ab22b0594d218d51fdfc1b
skuma274 <sandeep.e.kumar@capgemini.com>
2023-06-01 15:39:25 +0530

    added all health indicators

--- src/constants/health.endpoints.ts
+++ src/constants/health.endpoints.ts
@@ -1,7 +1,7 @@
-import path from "path";
-import { Transport } from "@nestjs/microservices";
+import path from 'path';
+import { Transport } from '@nestjs/microservices';
 
-export const HTTP_PING_URL = "http://localhost:3000/";
-export const MICROSERVICE_OPTION = { transport: Transport.TCP, option: { host: "localhost", port: 3000 } };
+export const HTTP_PING_URL = 'http://localhost:3000/';
+export const MICROSERVICE_OPTION = { transport: Transport.TCP, option: { host: 'localhost', port: 3000 } };
 export const MEMORY_THRESHOLD = 150 * 1024 * 1024;
-export const STORAGE_OPTION = { path: path.resolve('/'), thresholdPercent: 0.5 }
+export const STORAGE_OPTION = { path: path.resolve('/'), thresholdPercent: 0.5 };
