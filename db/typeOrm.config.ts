b300f439e527bb4b9c69eaf0c98939a47c5973cd
skuma274 <sandeep.e.kumar@capgemini.com>
2023-06-06 14:50:08 +0530

    fixed config naming

--- db/typeOrm.config.ts
+++ db/typeOrm.config.ts
@@ -3,15 +3,15 @@ import path from 'path';
 import { DataSource } from 'typeorm';
 import Config  from '../src/configs/app.configuration';
 
-const MConfig = Config() as Record<string, any>;
+const config = Config() as Record<string, any>;
 
 export  const AppDataSource = new DataSource({
     type: 'postgres',
-    host: MConfig.db.postgres.host,
-    port: MConfig.db.postgres.port,
-    database: MConfig.db.postgres.name,
-    username: MConfig.db.postgres.username,
-    password: MConfig.db.postgres.password,
+    host: config.db.postgres.host,
+    port: config.db.postgres.port,
+    database: config.db.postgres.name,
+    username: config.db.postgres.username,
+    password: config.db.postgres.password,
     entities:[path.join(__dirname, '..', 'src', 'modules', '**', '*.entity.ts')],
     migrations: [path.join(__dirname, 'migrations', '*.ts')]
 });
